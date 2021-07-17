f172ab26750621f5c655cb5448be1ecdd16b2c8b
Manpreet <manpreetkaurjagraon01@gmail.com>
2021-07-17 23:03:52 +0530

    this is manpreet's solution

---
+++ lab-solutions/js-conditionals/passwordChecker/passwordChecker.js
@@ -0,0 +1,11 @@
+var password = 7;
+
+if(password > 12){
+    console.log("Too long")
+}
+else if (password < 8) {
+         console.log("Too short")
+}
+ else{
+         console.log("Just right")
+}
