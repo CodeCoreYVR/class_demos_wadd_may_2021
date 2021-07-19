cda4c5197309421f28c7f925463f39990138164e
Rubalpreet kaur <84393565+Rubalpreet832@users.noreply.github.com>
2021-07-19 15:49:53 +0530

    Update index.js

--- lab-solutions/js-functions/reverseAString/index.js
+++ lab-solutions/js-functions/reverseAString/index.js
@@ -3,3 +3,21 @@ function reverseString(str)
     return str;
 }
 console.log(reverseString("hello"));
+//rubal codes you can also reverseString by using split and join method
+
+
+var string="hello";
+
+function reverseString(string) {
+    
+    var splitString = string.split(""); 
+ 
+    
+    var reverseArray = splitString.reverse(); 
+ 
+   
+    var joinArray = reverseArray.join(""); 
+        return joinArray; 
+}
+ 
+console.log(reverseString(string));
